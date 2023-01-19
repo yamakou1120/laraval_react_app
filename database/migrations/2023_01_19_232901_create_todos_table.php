e80395b626689accd783e114c49aefe24b56207f
yamakou1120 <kou.yama.2001.20@gmail.com>
2023-01-20 00:37:43 +0900

    データベース作成

---
+++ database/migrations/2023_01_19_232901_create_todos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //todos
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('title', 10);
+            $table->string('memo',50)->nullable();
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+};
---
+++ database/migrations/2023_01_19_233606_create_schedules_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //schedules
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('title',15);
+            $table->dateTime('start_time')->nullable();
+            $table->dateTime('end_time')->nullable();
+            $table->string('memo',50)->nullable();
+            $table->boolean('all_day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
